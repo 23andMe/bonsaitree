cf875f2a782cd1616979b03303ef3b5fdd163145
Ethan Jewett <ejewett@23andme.com>
2022-04-04 10:38:06 -0400

    Change remove_symmetric_ancestors() to consider relative sets when deciding on symmetrical-ness

--- bonsaitree/connect_pedigree_tools.py
+++ bonsaitree/connect_pedigree_tools.py
@@ -308,12 +308,28 @@ def remove_symmetric_ancestors(
 ) -> Set[int]:
     """
     Find out if any pair of ids in open_set are spouses. Remove one of them if neither is
-    genotyped. Because any pedigree created by joining through one ancestor will be the
-    same as the pedigree created by joining through their spouse.
+    genotyped and if they have identical relative sets. In this case, any pedigree created 
+    by joining through one ancestor will be the same as the pedigree created by joining 
+    through their spouse.
     Args:
         open_set: set of fully open ancestors obtained from get_open_ancestor_set() or
                   get_open_ancestor_set_for_leaves()
         po: pedigree object in which node_id resides
+    """
+    return_set = copy.copy(open_set)
+    for uid,info in po.up_pedigree_dict.items():
+        pid_set = {p for p in info[2:] if p is not None and p < 0} # ungenotyped, non-None ancestors
+        overlap_set = return_set & pid_set
+        if len(overlap_set) == 2:
+            p1,p2 = [*overlap_set]
+            p1_rels = po.rel_dict[p1]['anc'] | po.rel_dict[p1]['rel'] | po.rel_dict[p1]['desc']
+            p2_rels = po.rel_dict[p2]['anc'] | po.rel_dict[p2]['rel'] | po.rel_dict[p2]['desc']
+            # If p1_rels and p2_rels are the same, then p1 and p2 are indistinguishable
+            if p1_rels == p2_rels:
+                del_p = sorted(overlap_set)[0] # for reproducibility, always drop the smallest ID
+                return_set.remove(del_p)
+    return return_set
+
     """
     return_set = copy.copy(open_set) # don't modify arguments
     for uid,info in po.up_pedigree_dict.items():
@@ -324,6 +340,7 @@ def remove_symmetric_ancestors(
             save_id = ungenotyped_overlap_set.pop()
             return_set -= ungenotyped_overlap_set
     return return_set
+    """
 
 
 def get_connecting_founders_degs_and_log_likes(
@@ -609,8 +626,8 @@ def combine_pedigrees(
     gt_set1,gt_set2 = get_related_sets(gt_set1, gt_set2, ibd_seg_list)
 
     # gt_set1 may not have a single common ancestor due to background IBD/inbreeding etc.
-    # if necessary, find the common ancestor(s) whose descendants share the most with pedigree 2
-    # and retain their descendants as gt_set1
+    # if necessary, find the common ancestor (or mate-pair of ancestors) whose descendants 
+    #share the most with pedigree 2 and retain their descendants as gt_set1
     gt_set1 = get_best_desc_id_set(gt_set1, gt_set2, po1, ibd_seg_list)
     gt_set2 = get_best_desc_id_set(gt_set2, gt_set1, po2, ibd_seg_list)
 
